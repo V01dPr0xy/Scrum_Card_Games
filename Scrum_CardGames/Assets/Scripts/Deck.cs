33d07911073af5f396b84d4be8ef0a6894a5403b
bdorman <bdorman@student.neumont.edu>
2018-05-29 13:30:18 -0600

    Working on the shuffle method

--- Scrum_CardGames/Assets/Scripts/Deck.cs
+++ Scrum_CardGames/Assets/Scripts/Deck.cs
@@ -7,11 +7,11 @@ public class Deck : MonoBehaviour
 	public enum eDeckTupe { STANDARD, FULL, CUSTOM }
 
 	List<int> m_cards;
-	
+
 
 	public void Build(eDeckTupe type = eDeckTupe.STANDARD, List<int> included = null) //the list needs to be of cards
 	{
-		switch(type)
+		switch (type)
 		{
 			case eDeckTupe.STANDARD:
 				break;
@@ -25,8 +25,31 @@ public class Deck : MonoBehaviour
 	public void Shuffle()
 	{
 		List<int> deck = new List<int>();
-		int[] indexes = new int[m_cards.Count];
+		List<int> indexes = new List<int>();
 
-		
+		//loop through cards
+		foreach (int card in m_cards)
+		{
+			//setup lock loop
+			while (true)
+			{
+				//generate random index
+				int test = Random.Range(0, m_cards.Count - 1);
+				int misses = 0;
+				//loop through indexes
+				foreach (int i in indexes)
+				{
+					//check if index is valid
+					if (i != test) ++misses;
+					else break;
+				}
+				//if true record index and add card[index] to new deck break from lock loop
+				if(misses == indexes.Count)
+				{
+					indexes.Add(test);
+
+				}
+			}
+		}
 	}
 }
