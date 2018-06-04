e33a2a677d0bb4ba862194e573bef3147c91ccd0
Wesley Alnor <WAlnor@student.neumont.edu>
2018-06-04 11:26:20 -0600

    Deck prefab and more war work

--- Scrum_CardGames/Assets/Scripts/Game Scripts/WarGameController.cs
+++ Scrum_CardGames/Assets/Scripts/Game Scripts/WarGameController.cs
@@ -8,13 +8,31 @@ public class WarGameController : MonoBehaviour {
     [SerializeField] GameObject WinTXTp1;
     [SerializeField] GameObject WinTXTp2;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] Card_Placement P1Deck;
+    [SerializeField] Card_Placement P2Deck;
+
+    [SerializeField] Deck m_Deck;
+
+    public void newGame()
+    {
+        m_Deck.Dropbox();
+
+        bool GiveP1 = true;
+
+        foreach (Card c in m_Deck.Cards)
+        {
+            if (GiveP1)
+            {
+                P1Deck.GiveCard(c);
+            }
+            else
+                P2Deck.GiveCard(c);
+
+            c.gameObject.transform.localScale = new Vector3(1,1,1);
+
+            GiveP1 = !GiveP1;
+        }
+
+        m_Deck.Cards.Clear();
+    }
 }
